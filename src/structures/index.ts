164f4f9a3f6584395bbb783d1c65436d504dd2e0
gooddltmdqls <1415wwwh@gmail.com>
2024-12-13 20:24:20 +0900

    Export it

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -1,7 +1,8 @@
 import "reflect-metadata";
 
 export * from "./decorators";
+export * from "./components";
+
 export * from "./Constants";
 export * from "./DiscordApp";
 export * from "./Plugin";
-export * from "./Util";
